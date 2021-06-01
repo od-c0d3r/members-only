52f597968cb7bb9e45f194355603d73983bd21af
od-c0d3r <omar_rashad@aol.com>
2021-06-01 15:23:20 +0200

    Crearing Post model

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+
+    # skip_before_action :require_login, only: [:new, :create]
+    
+    def index
+        
+    end
+    
+
+    def new
+        
+    end
+    
+
+    # private
+
+    # def require_login
+    #   unless logged_in?
+    #     flash[:error] = "You must be logged in to access this section"
+    #     redirect_to new_login_url # halts request cycle
+    #   end
+    # end
+
+end
