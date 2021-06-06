0f5c5940b82ffbb127adbea8966cdcdf3801dde4
od-c0d3r <omar_rashad@aol.com>
2021-06-06 11:16:33 +0200

    Enabling Bootstrap Powers

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ ruby '3.0.1'
 gem 'rails', '~> 6.1.3', '>= 6.1.3.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
+gem 'bootstrap-sass', '3.4.1'
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    def full_title(page_title)
+        base_title = 'Members Only Blog'
+        if page_title.empty?
+            base_title
+        else
+            page_title + ' | ' + base_title
+        end
+    end
 end
